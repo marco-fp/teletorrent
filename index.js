480e6e477a92a3bc7cb2b21fda337c6977aed9cd
MarFerPra <marco.fp@94@gmail.com>
2016-09-26 18:59:00 +0200

    [T1] Recibir y almacenar archivo .torrent

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var utils = require('./utils')
 var TelegramBot = require('node-telegram-bot-api');
 
 var bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
@@ -5,4 +6,16 @@ var bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 bot.onText(/\/start/, function (msg) {
   var chatId = msg.chat.id;
   bot.sendMessage(chatId, "Hi, I'm TeleTorrent. \nI'm here to help you download highly legal multimedia content from the information superhighway.");
+});
+
+bot.on('message', function(msg){
+  var chatId = msg.chat.id;
+  var fileId = msg.document.file_id;
+  var mimeType = msg.document.mime_type;
+  if(utils.matchesTorrent(mimeType)){
+    bot.downloadFile(fileId, "tmp");
+    bot.sendMessage(chatId, "Just received "+msg.document.file_name+". Should I start downloading it?");
+  } else {
+    bot.sendMessage(chatId, "File: \""+msg.document.file_name+"\" is not a torrent file or a magnet link.");
+  }
 });
