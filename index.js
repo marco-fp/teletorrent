4caec9acf6762571ec7c32da56802de1bfa67b03
marcofp <marco.fp94@gmail.com>
2016-10-26 23:05:23 +0200

    Help menu, refactor into functions to easily launch tests.

--- index.js
+++ index.js
@@ -11,68 +11,29 @@ mongoose.connect('mongodb://localhost/testdb', function(error) {
     }
 });
 
-var Download = require('./models/download');
-
 var bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
-var opts = {
-  reply_markup: JSON.stringify(
-    {
-      force_reply: true
-    }
-)};
-
 bot.onText(/\/start/, function (msg) {
-  var chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Hi, I'm TeleTorrent. \nI'm here to help you download highly legal multimedia content from the information superhighway.");
+  utils.answerStart(msg, function(res){
+    bot.sendMessage(msg.chat.id, res);
+  });
+});
+bot.onText(/\/help/, function (msg) {
+  utils.answerHelp(msg, function(res){
+    bot.sendMessage(msg.chat.id, res);
+  });
 });
 
 bot.on('message', function(msg){
-  console.log(msg);
-  processMessage(msg);
+  processMessage(msg, this);
 });
 
-processMessage = function(msg) {
-    var chatId = msg.chat.id;
+processMessage = function(msg, bot) {
     if(typeof(msg.document) !== 'undefined'){
-      var fileId = msg.document.file_id;
-      var mimeType = msg.document.mime_type;
-
-      var fileName = msg.document.file_name;
-      var fileSize = msg.document.file_size;
-
-      if(utils.matchesTorrent(mimeType)){
-        bot.sendMessage(chatId, "Just received "+msg.document.file_name+". Should I start downloading it?", opts).then( function(sended) {
-          var chatId = sended.chat.id;
-          var messageId = sended.message_id;
-          bot.onReplyToMessage(chatId, messageId, function (message) {
-            if(message.text === 'yes' || message.text === 'Yes'){
-              bot.downloadFile(fileId, "tmp");
-
-              var fileAddedAt = message.date;
-
-              var download = new Download();
-              download.fileName = fileName;
-              download.fileSize = fileSize;
-              download.addedAt = fileAddedAt;
-
-              download.save(function(error) {
-                if (error) {
-                  bot.sendMessage(chatId, "An error occurred while proccessing the torrent.");
-                } else {
-                  bot.sendMessage(chatId, "Torrent saved, starting download.");
-                }
-              });
-
-
-            } else {
-              bot.sendMessage(chatId, "Unknown answer, sorry.");
-            }
-          });
-        });
-      } else {
-        bot.sendMessage(chatId, "File: \""+msg.document.file_name+"\" is not a torrent file or a magnet link.");
-      }
+      utils.processDocument(msg, bot);
+    } else {
+      utils.menuSelector(msg.text, function(res){
+        bot.sendMessage(msg.chat.id, res);
+      });
     }
-
 }
