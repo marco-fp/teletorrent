cc3f24ba01aaf0787f7c6d2f21acddbbeafe45af
MarFerPra <mfernandezpranno@gmail.com>
2017-01-29 20:43:02 +0100

    Deploy files

--- fabfile.py
+++ fabfile.py
@@ -5,20 +5,20 @@ env.host = ['marco:22']
 
 def start():
     with shell_env(BOT_TOKEN=os.environ['BOT_TOKEN'], DB_PASSWORD=os.environ['DB_PASSWORD'], DB_USER=os.environ['DB_USER']):
-        run ('cd teletorrent && npm start')
+        run ('sudo cd teletorrent && sudo npm start')
 
 def clone():
     run ('sudo rm -rf teletorrent')
     run ('sudo git clone https://github.com/MarFerPra/teletorrent')
 
 def remove():
-    run ('rm -rf teletorrent')
+    run ('sudo rm -rf teletorrent')
 
 def test():
-    run ('cd teletorrent && npm test')
+    run ('sudo cd teletorrent && sudo npm test')
 
 def install():
-    run ('cd teletorrent && make install')
+    run ('sudo cd teletorrent && sudo make install')
 
 def ping():
     run('echo Ping_received')
