b9de3a2412beb05595ec6ff3070b62df834fc32a
MarFerPra <mfernandezpranno@gmail.com>
2017-01-29 22:23:54 +0100

    Deployment

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ env.host = ['marco:22']
 
 def start():
     with shell_env(BOT_TOKEN=os.environ['BOT_TOKEN'], DB_PASSWORD=os.environ['DB_PASSWORD'], DB_USER=os.environ['DB_USER']):
-        run ('sudo cd teletorrent && sudo npm start')
+        run ('cd teletorrent && sudo npm start')
 
 def clone():
     run ('sudo rm -rf teletorrent')
@@ -15,10 +15,10 @@ def remove():
     run ('sudo rm -rf teletorrent')
 
 def test():
-    run ('sudo cd teletorrent && sudo npm test')
+    run ('cd teletorrent && sudo npm test')
 
 def install():
-    run ('sudo cd teletorrent && sudo make install')
+    run ('cd teletorrent && sudo make install')
 
 def ping():
     run('echo Ping_received')
