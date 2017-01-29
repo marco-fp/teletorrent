a743b55b6d34afca7063cc3278b2dc0f40d9245f
MarFerPra <mfernandezpranno@gmail.com>
2017-01-29 22:47:37 +0100

    Deploy

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ import os
 env.host = ['marco:22']
 
 def start():
-    with shell_env(BOT_TOKEN=os.environ['BOT_TOKEN'], DB_PASSWORD=os.environ['DB_PASSWORD'], DB_USER=os.environ['DB_USER']):
+    with shell_env(BOT_TOKEN=os.environ['BOT_TOKEN'], DB_PASSWORD=os.environ['DB_PASSWORD'], DB_USER=os.environ['DB_USER'], NODE_ENV=os.environ['NODE_ENV']):
         run ('cd teletorrent && npm start')
 
 def clone():
